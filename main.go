0d4eada02466a8c219d792e0ac3cc23be925bcc5
Roman Volosatovs <rvolosatovs@riseup.net>
2017-09-26 14:08:52 +0200

    protoc-gen-gogottn: add

---
+++ main.go
@@ -0,0 +1,45 @@
+// Copyright © 2017 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
+
+package main
+
+import (
+	"github.com/gogo/protobuf/protoc-gen-gogo/descriptor"
+	"github.com/gogo/protobuf/vanity"
+	"github.com/gogo/protobuf/vanity/command"
+)
+
+func main() {
+	req := command.Read()
+	files := req.GetProtoFile()
+	files = vanity.FilterFiles(files, vanity.NotGoogleProtobufDescriptorProto)
+
+	for _, opt := range []func(*descriptor.FileDescriptorProto){
+		//vanity.TurnOffGoGettersAll,
+		//vanity.TurnOffGoEnumPrefixAll,
+		vanity.TurnOffGoStringerAll,
+		vanity.TurnOnVerboseEqualAll,
+		//vanity.TurnOnFaceAll,
+		//vanity.TurnOnGoStringAll,
+		vanity.TurnOnPopulateAll,
+		vanity.TurnOnStringerAll,
+		vanity.TurnOnEqualAll,
+		//vanity.TurnOnDescriptionAll,
+		vanity.TurnOnTestGenAll,
+		vanity.TurnOnBenchGenAll,
+		vanity.TurnOnMarshalerAll,
+		vanity.TurnOnUnmarshalerAll,
+		//vanity.TurnOnStable_MarshalerAll,
+		vanity.TurnOnSizerAll,
+		//vanity.TurnOffGoEnumStringerAll,
+		//vanity.TurnOnEnumStringerAll,
+		//vanity.TurnOnUnsafeUnmarshalerAll,
+		//vanity.TurnOnUnsafeMarshalerAll,
+		//vanity.TurnOffGoExtensionsMapAll,
+		vanity.TurnOffGoUnrecognizedAll,
+		//vanity.TurnOffGogoImport,
+		 //vanity.TurnOnCompareAll,
+	} {
+		vanity.ForEachFile(files, opt)
+	}
+	command.Write(command.Generate(req))
+}
