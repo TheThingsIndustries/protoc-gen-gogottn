362987ee6e89d31ba25c57d51d2fc5e55ebc902c
Roman Volosatovs <rvolosatovs@riseup.net>
2019-01-07 10:05:51 +0100

    Update license

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright © 2018 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
+// Copyright © 2019 The Things Network Foundation, distributed under the MIT license (see LICENSE file)
 
 package main
 
